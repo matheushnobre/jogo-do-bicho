bb1a05415c9bdec8b0b5ddccc190fa55a88d08c4
Matheus Nobre <matheus1204h@gmail.com>
2026-02-19 17:43:33 -0300

    chore #1: configure spring boot

---
+++ bichofull-backend/bichofull/src/main/java/br/com/bichofull/bichofull/BichofullApplication.java
@@ -0,0 +1,13 @@
+package br.com.bichofull.bichofull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BichofullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BichofullApplication.class, args);
+	}
+
+}
---
+++ bichofull-backend/bichofull/src/test/java/br/com/bichofull/bichofull/BichofullApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bichofull.bichofull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BichofullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
