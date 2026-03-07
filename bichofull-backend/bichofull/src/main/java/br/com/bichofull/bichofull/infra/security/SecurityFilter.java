17851676e499d38e49d786ebc3307201d80a4243
Matheus Nobre <matheus1204h@gmail.com>
2026-03-07 11:10:46 -0300

    feat: filter for jwt in backend

--- bichofull-backend/bichofull/src/main/java/br/com/bichofull/bichofull/infra/security/SecurityFilter.java
+++ bichofull-backend/bichofull/src/main/java/br/com/bichofull/bichofull/infra/security/SecurityFilter.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -19,23 +20,31 @@ import java.io.IOException;
 public class SecurityFilter extends OncePerRequestFilter {
 
     @Autowired
-    TokenService tokenService;
+    private TokenService tokenService;
     
     @Autowired
-    UserRepository userRepository;
-    
+    private UserRepository userRepository;
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getServletPath();
+        return path.equals("/api/auth/login") ||
+                path.equals("/api/auth/register");
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = CookieService.getCookie(request, "token");
-
+        System.out.println("token recebido " + token);
         if(token != null && SecurityContextHolder.getContext().getAuthentication() == null){
-            var email = tokenService.validateToken(token);
+            String email = tokenService.validateToken(token);
 
             if(email != null) {
                 UserDetails user = userRepository.findByEmail(email);
 
                 if (user != null) {
                     var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                     SecurityContextHolder.getContext().setAuthentication(authentication);
                 }
             }
