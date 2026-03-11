91a1f9872c23e8782c2c064a0854ebfe6e34d048
Matheus Nobre <matheus1204h@gmail.com>
2026-03-11 20:29:07 -0300

    feat: add validations to inputs in bet-component

--- bichofull-frontend/src/app/components/bet/bet-component.ts
+++ bichofull-frontend/src/app/components/bet/bet-component.ts
@@ -13,16 +13,60 @@ export class BetComponent {
   selectedType: string = 'group';
   betNumber: string = '';
   betAmount: number | null = null;
+  numberErrorMessage: string = '';
+  amountErrorMessage: string = '';
+
+  private validationRules: any = {
+    group: { min: 1, max: 25, label: 'BICHO' },
+    dezena: { min: 0, max: 99, label: 'CENTENA' },
+    thousands: { min: 0, max: 9999, label: 'MILHAR' }
+  };
+
+  private validateBetNumber(){
+    const num = parseInt(this.betNumber);
+    const rule = this.validationRules[this.selectedType];
+
+    if(!this.betNumber){
+      this.numberErrorMessage = '';
+      return;
+    }
+
+    if(isNaN(num) || num < rule.min || num > rule.max){
+      this.numberErrorMessage = `O número para jogar ${rule.label} deve ser entre ${rule.min} e ${rule.max}`;
+    } else{
+      this.numberErrorMessage = '';
+    }
+  }
+
+  private validateAmountNumber(){
+    const num = this.betAmount;
+    if (num === null || num === undefined) {
+      this.amountErrorMessage = '';
+      return;
+    }
+
+    if(num <= 0){
+      this.amountErrorMessage = 'Valor apostado deve ser maior que 0'
+    }
+  }
+
+  validateInput(){
+    this.validateBetNumber();
+    this.validateAmountNumber();
+  }
 
   setType(type: string) {
     this.selectedType = type;
+    this.validateInput();
   }
 
   setAmount(val: number) {
     this.betAmount = val;
+    this.validateInput();
   }
 
   confirmBet() {
+    this.validateInput();
     console.log('Aposta confirmada:', {
       tipo: this.selectedType,
       numero: this.betNumber,
