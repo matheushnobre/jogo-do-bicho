71262de8251f1eba347da4981d9d8b8fb49c9a67
Matheus Nobre <matheus1204h@gmail.com>
2026-03-16 14:49:09 -0300

    bugfix: adjust resultdto

--- bichofull-backend/bichofull/src/main/java/br/com/bichofull/bichofull/domain/results/ResultDTO.java
+++ bichofull-backend/bichofull/src/main/java/br/com/bichofull/bichofull/domain/results/ResultDTO.java
@@ -3,14 +3,14 @@ package br.com.bichofull.bichofull.domain.results;
 import java.util.List;
 
 public record ResultDTO(
-        DrawDTO mainDraw,
-        List<DrawDTO> secondaryDraws
+        Draw mainDraw,
+        List<Draw> secondaryDraws
 ) {
 
     public ResultDTO(Result result){
         this(
-                new DrawDTO(result.getMainDraw()),
-                result.getSecondaryDraws().stream().map(DrawDTO::new).toList()
+                result.getMainDraw(),
+                result.getSecondaryDraws()
         );
     }
 }
