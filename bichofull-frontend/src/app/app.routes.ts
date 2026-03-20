6f511624a9358cc5663ad180f143ac5e6a690b95
Matheus Nobre <matheus1204h@gmail.com>
2026-03-19 21:24:19 -0300

    feat: add history page

--- bichofull-frontend/src/app/app.routes.ts
+++ bichofull-frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from './components/pages/login/login-component';
 import { RegisterComponent } from './components/pages/register/register-component';
 import { HomeComponent } from './components/pages/home/home-component';
 import { authGuard } from './guards/auth-guard';
+import { HistoryBetsComponent } from './components/pages/history-bets/history-bets-component';
 
 export const routes: Routes = [
     { path: "login", component: LoginComponent},
     { path: "register", component: RegisterComponent},
     { path: "home", component: HomeComponent, canActivate: [authGuard]},
+    { path: "historico", component: HistoryBetsComponent, canActivate: [authGuard]},
     { path: "**", redirectTo: "home" },
 ];
